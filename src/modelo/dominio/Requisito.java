b4f063a4e52caaba9b9c3bd3a402adccb9ddde71
miguel <miguelmombelli@gmail.com>
2021-11-06 14:01:24 -0300

    requisito

---
+++ src/modelo/dominio/Requisito.java
@@ -0,0 +1,17 @@
+package modelo.dominio;
+public class requisito{
+	String nombre;
+
+	//Constructores:
+		public requisito(String nombre){
+			this.nombre = nombre;
+
+		}
+		
+	// Setters y Getters:
+		public void setNombre(String nombre){
+			this.nombre = nombre;
+		}
+		public String getNombre(){
+			return this.nombre;
+}
