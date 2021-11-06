4d7fd188af6c3d8c95aa1dcdb99fbf36038fa2bc
miguel <miguelmombelli@gmail.com>
2021-11-06 13:33:16 -0300

    revisar las list

--- src/modelo/dominio/FormularioCarga.java
+++ src/modelo/dominio/FormularioCarga.java
@@ -1,8 +1,8 @@
 package modelo.dominio;
 public class FormularioCarga{
 
-	Byte cv;
-	float remuneracion;
+	private Byte cv;
+	private float remuneracion;
 
 	// Constructores:
 		public FormularioCarga(Byte cv,float remuneracion){
@@ -12,6 +12,24 @@ public class FormularioCarga{
 		}
 
 	// Setters y Getters:
-		
+		public Byte getCV(){
+			return cv;
+		}
 
+		public void setCV(Byte cv){
+
+			this.cv = cv;
+
+		}
+
+		public float getRemuneracion(){
+
+			return remuneracion;
+		}
+
+		public void setRemuneracion(float remuneracion){
+
+			this.remuneracion = remuneracion;
+		}
+		
 }
