ff4dff1b6645cb994e61f20e36ae46dffbaa27c5
Franco <fragrodriguez@gmail.com>
2021-11-06 20:29:01 -0300

    Controlador terminado con buscadores

--- src/controlador/ControladorConfiguracion.java
+++ src/controlador/ControladorConfiguracion.java
@@ -5,35 +5,71 @@ import modelo.dominio.Categoria;
 public class ControladorConfiguracion{
 	private List<Idioma> idiomas;
 	private List<Pais> paises;
-	private List<Categoria> categoria;
+	private List<Categoria> categorias;
 
 	// Constructores
 		public ControladorConfiguracion(){
 			this.idiomas = new List<Idioma>();
 			this.paises = new List<Pais>();
-			this.categoria = new List<Categoria>();
+			this.categorias = new List<Categoria>();
 		}
 	// Collections:
-		public void agregarIdioma(Idioma idiomas){
-			this.Idiomas.add(idioma);
+		public void agregarIdioma(String idioma){
+			this.idiomas.add(new Idioma(idioma));
+		}
+		private Idioma buscarIdioma(String nombre){
+			Idioma resultado;
+			for (Idioma idioma : idiomas) {
+				if (idioma.getNombre() == nombre){
+					resultado = idioma;
+					break;
+				}
+			}
+			return resultado
 		}
 
-		public void eliminarIdioma(Idioma idiomas){
-			this.Idiomas.remove(idioma);
+		public void eliminarIdioma(String idioma){
+			Idioma elemento = this.buscarIdioma(idioma);
+			if (elemento != null)
+				this.idiomas.remove(elemento);
 		}
 
-		public void agregarPais(Pais paises){
-			this.Pais.add(paises);
+		public void agregarPais(String pais){
+			this.paises.add(new Pais(pais));
+		}
+		private Pais buscarPais(String nombre){
+			Pais resultado;
+			for (Pais pais : paises) {
+				if (pais.getNombre() == nombre){
+					resultado = pais;
+					break;
+				}
+			}
+			return resultado
 		}
 
-		public void eliminarPais(Pais paises){
-			this.Pais.remove(paises);
+		public void eliminarPais(String paises){
+			Pais elemento = this.buscarPais(pais);
+			if (elemento != null)
+				this.paises.remove(elemento);
 		}
-		public void agregarCategoria(Categoria categoria){
-			this.Categoria.add(categoria);
+		public void agregarCategoria(String categoria){
+			this.categorias.add(new Categoria(categoria));
+		}
+		private Categoria buscarCategoria(String nombre){
+			Categoria resultado;
+			for (Categoria categoria : categorias) {
+				if (categoria.getNombre() == nombre){
+					resultado = categoria;
+					break;
+				}
+			}
+			return resultado
 		}
 
-		public void eliminarCategoria(Categoria categoria){
-			this.Categoria.remove(categoria);
+		public void eliminarCategoria(String categoria){
+			Categoria elemento = this.buscarCategoria(categoria);
+			if (elemento != null)
+				this.categorias.remove(elemento);
 		}
 }
