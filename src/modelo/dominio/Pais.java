a75b6e086cc5dca47c7f1d31e85f953ea4891da6
Franco <fragrodriguez@gmail.com>
2021-11-06 19:47:31 -0300

    País ya no tiene idioma

--- src/modelo/dominio/Pais.java
+++ src/modelo/dominio/Pais.java
@@ -1,12 +1,10 @@
 package modelo.dominio;
 public class Pais {
 	String nombre;
-	String idioma;
 
 	// Constructores:
-		public Pais(String nombre, String idioma){
+		public Pais(String nombre){
 			this.nombre = nombre;
-			this.idioma = idioma;
 		}
 	// Setters y Getters:
 		public void setNombre(String nombre){
@@ -14,11 +12,5 @@ public class Pais {
 		}
 		public String getNombre(){
 			return this.nombre;
-		}
-		public void setIdioma(String idioma){
-			this.idioma = idioma;
-		}
-		public String getIdioma(){
-			return this.idioma;
 		}
 }
