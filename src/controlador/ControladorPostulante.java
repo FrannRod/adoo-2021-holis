2ab499ed4e0faffe08bb216b498d95172eed7190
Franco <fragrodriguez@gmail.com>
2021-11-06 22:48:08 -0300

    Controlador postulante

---
+++ src/controlador/ControladorPostulante.java
@@ -0,0 +1,66 @@
+package controlador;
+import modelo.dominio.Postulante;
+import modelo.vo.PostulanteVO;
+import java.util.List;
+public class ControladorPostulante{
+	private List<Postulante> postulantes;
+	private static ControladorPostulante instancia;
+	// Constructores
+		private ControladorPostulante(){
+			this.postulantes = new List<Postulante>();
+		}
+		public ControladorPostulante getInstancia(){
+			if (this.instancia == null)
+				this.instancia = new ControladorPostulante();
+			return instancia;
+		}
+	// Collections:
+		public void agregarPostulante(PostulanteVO vo){
+			Postulante postulante = new Postulante( vo.nombre,
+											vo.apellido,
+											vo.fechaDeNacimiento,
+											vo.nacionalidad,
+											vo.usuario,
+											vo.contraseña
+										  );
+			this.postulantes.add(postulante);
+		}
+		protected Postulante buscarPostulante(PostulanteVO vo){
+			Postulante resultado;
+			for (Postulante postulante : postulantes) {
+				if (Postulante.getNombre() == vo.getNombre() && Postulante.getApellido() == vo.getApellido()){
+					resultado = postulante;
+					break;
+				}
+			}
+			return resultado
+		}
+		public void eliminarPostulante(PostulanteVO vo){
+			Postulante elemento = this.buscarPostulante(vo);
+			if (elemento != null)
+				this.postulantes.remove(elemento);
+		}
+		public boolean modificarPostulante(PostulanteVO vo){
+			Postulante postulante = this.buscarPostulante(vo);
+			if (postulante == null)
+				return false;
+			postulante.setNombre(vo.nombre);
+			postulante.setApellido(vo.apellido);
+			postulante.setFechaDeNacimiento(vo.fechaDeNacimiento);
+			postulante.setNacionalidad(vo.nacionalidad);
+			postulante.setUsuario(vo.usuario);
+			postulante.setContraseña(vo.contraseña);
+			return true;
+		}
+
+		public boolean agregarInteres(PostulanteVO vo, String categoria){
+			Postulante postulante = this.buscarPostulante(vo);
+			if (postulante == null)
+				return false;
+			Categoria cat = ControladorConfiguracion.getInstancia().buscarCategoria(categoria);
+			if (cat == null)
+				return false;
+			postulante.agregarInteres(cat);
+			return true;
+		}
+}
---
+++ src/modelo/vo/PostulanteVO.java
@@ -0,0 +1,66 @@
+package modelo.vo;
+public class PostulanteVO{
+
+	private String nombre;
+	private String apellido;
+	private Date fechaDeNacimiento;
+	private Pais nacionalidad;
+	private String usuario;
+	private String contraseña;
+
+	// Constructores
+		public PostulanteVO(String nombre,String apellido,Date fechaDeNacimiento,Pais nacionalidad,String usuario,String contraseña){
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.fechaDeNacimiento = fechaDeNacimiento;
+			this.nacionalidad = nacionalidad;
+			this.usuario = usuario;
+			this.contraseña = contraseña;
+		}
+
+	// Setters y Getters:
+		public String getNombre(){
+			return nombre;
+		}
+
+		public void setNombre(String nombre){
+			this.nombre = nombre;
+		}
+
+		public String getApellido(){
+			return apellido;
+		}
+
+		public void setApellido(String apellido){
+			this.apellido = apellido;
+		}
+
+		public Date getFechaDeNacimiento(){
+			return fechaDeNacimiento;
+		}
+
+		public void setFechaDeNacimiento(){
+			this.fechaDeNacimiento = fechaDeNacimiento;
+		}
+
+		public Pais getNacionalidad(){
+			return nacionalidad;
+		}
+
+		public void setNacionalidad(Pais nacionalidad){
+			this.nacionalidad = nacionalidad;
+		}
+
+		public String getContraseña(){
+			return contraseña;
+		}
+		public void setContraseña(String contraseña){
+			this.contraseña = contraseña;
+		}
+		public String getUsuario(){
+			return usuario;
+		}
+		public void setUsuario(String usuario){
+			this.usuario = usuario;
+		}
+}
