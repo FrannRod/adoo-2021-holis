8230ab714dc22d94c9c00cc14276b2ca3adcb868
Franco <fragrodriguez@gmail.com>
2021-11-09 21:39:37 -0300

    Main

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,6 +1,10 @@
 package test;
+import controlador.ControladorEmpresa;
+import modelo.vo.EmpresaVO;
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		EmpresaVO nuevaEmpresa = new EmpresaVO("La lechita feliz S.A.", 216665514, null, "leche@feliz.com", "4749-212", "Sociedad Anónima", "1234", "lechefeliz");
+		ControladorEmpresa controladorEmpresa = ControladorEmpresa().getInstancia();
+		controladorEmpresa.agregarEmpresa(nuevaEmpresa);
 	}
 }
