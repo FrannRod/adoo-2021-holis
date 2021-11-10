469c54fcac69683703958e8a9011f007121ce6b0
sophia-dellarciprete <90946600+sophia-dellarciprete@users.noreply.github.com>
2021-11-09 22:32:52 -0300

    Ahora en serio está Tarea

--- src/modelo/dominio/Tarea.java
+++ src/modelo/dominio/Tarea.java
@@ -1,12 +1,12 @@
 package modelo.dominio;
 public class Tarea{
-    String descripcion
+    private String descripcion
 
-	public Object getDescripcion() {
+	public String getDescripcion() {
 		return this.descripcion;
 	}
 
-	public void setDescripcion(Object descripcion) {
+	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 ;
