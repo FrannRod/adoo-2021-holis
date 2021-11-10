170cb91a2e777d8d5500a77f0e18ec17945b9695
Franco <fragrodriguez@gmail.com>
2021-11-09 23:02:25 -0300

    Empresa La lechita feliz S.A. creada

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,9 +1,11 @@
 package test;
 import controlador.ControladorEmpresa;
 import modelo.vo.EmpresaVO;
+import modelo.vo.DireccionVO;
 public class Main {
 	public static void main(String[] args) {
-		EmpresaVO nuevaEmpresa = new EmpresaVO("La lechita feliz S.A.", 216665514, null, "leche@feliz.com", "4749-212", "Sociedad Anónima", "1234", "lechefeliz");
+		DireccionVO direccion = new DireccionVO("String codigoPostal","String nombrePais","String localidad","String calle",2,1);
+		EmpresaVO nuevaEmpresa = new EmpresaVO("La lechita feliz S.A.", 216665514, direccion, "leche@feliz.com", "4749-212", "Sociedad Anónima", "1234", "lechefeliz");
 		ControladorEmpresa controladorEmpresa = ControladorEmpresa.getInstancia();
 		controladorEmpresa.agregarEmpresa(nuevaEmpresa);
 	}
